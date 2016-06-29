f18ee15b308bf7008c7f70dfc95292eda196cf1f
spawn150 <spawn150@gmail.com>
2016-06-29 08:53:51 +0200

    naming refactor

--- app/src/main/java/com/example/architecture/bad/myfigurecollection/util/StringUtils.java
+++ app/src/main/java/com/example/architecture/bad/myfigurecollection/util/StringUtils.java
@@ -1,45 +1,43 @@
 package com.example.architecture.bad.myfigurecollection.util;
 
-import android.text.TextUtils;
-
 /**
  * Created by spawn on 28/06/16.
  */
 public class StringUtils {
 
-    public static String extractStringBeforeSeparatorRepeatedNTimes(String value, char separator, int repetition) {
+    public static String extractStringBeforeSeparatorRepeatedNTimes(String value, char separator, int times) {
 
         if (value == null || "".equals(value)) {
             return "";
         }
 
-        int index = extractIndexOfSepratorRepeated(value, separator, 0, repetition, 0);
+        int index = extractIndexOfSeparatorRepeatedNTimes(value, separator, 0, times, 0);
 
         if(index == -1) return value;
 
         return value.substring(0, index).trim();
     }
 
-    public static String extractStringAfterSeparatorRepeatedNTimes(String value, char separator, int repetition) {
+    public static String extractStringAfterSeparatorRepeatedNTimes(String value, char separator, int times) {
 
         if (value == null || "".equals(value)) {
             return "";
         }
 
-        int index = extractIndexOfSepratorRepeated(value, separator, 0, repetition, 0);
+        int index = extractIndexOfSeparatorRepeatedNTimes(value, separator, 0, times, 0);
 
         if(index == -1) return value;
 
         return value.substring(++index).trim();
     }
 
-    private static int extractIndexOfSepratorRepeated(String name, char separator, int start, int repetition, int acc) {
+    private static int extractIndexOfSeparatorRepeatedNTimes(String name, char separator, int start, int times, int acc) {
 
-        if (repetition == acc) return --start;
+        if (times == acc) return --start;
 
         int indexNSeparator = name.indexOf(separator, start);
 
-        return (indexNSeparator == -1) ? --start : extractIndexOfSepratorRepeated(name, separator, ++indexNSeparator, repetition, ++acc);
+        return (indexNSeparator == -1) ? --start : extractIndexOfSeparatorRepeatedNTimes(name, separator, ++indexNSeparator, times, ++acc);
 
     }
 
--- app/src/test/java/com/example/architecture/bad/myfigurecollection/StringUtilsUnitTest.java
+++ app/src/test/java/com/example/architecture/bad/myfigurecollection/StringUtilsUnitTest.java
@@ -9,7 +9,7 @@ import static org.junit.Assert.assertEquals;
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
-public class ExampleUnitTest {
+public class StringUtilsUnitTest {
 
     @Test
     public void testExtractStringBefore() throws Exception {
