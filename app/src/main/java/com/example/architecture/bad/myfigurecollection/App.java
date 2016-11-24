28b92a120064a651255ef02e536d97d05daa71ca
spawn150 <spawn150@gmail.com>
2016-11-24 15:22:39 +0100

    add Picasso initialization in Application class

--- app/src/main/java/com/example/architecture/bad/myfigurecollection/App.java
+++ app/src/main/java/com/example/architecture/bad/myfigurecollection/App.java
@@ -28,7 +28,6 @@ public class App extends Application {
         Picasso.Builder builder = new Picasso.Builder(this);
         builder.downloader(new OkHttpDownloader(this, Integer.MAX_VALUE));
         Picasso built = builder.build();
-        //built.setIndicatorsEnabled(BuildConfig.DEBUG);
         built.setLoggingEnabled(BuildConfig.DEBUG);
         Picasso.setSingletonInstance(built);
     }
