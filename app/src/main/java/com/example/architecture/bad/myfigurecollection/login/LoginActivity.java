fdcf92169f41b6992d469478e95ed6648bd92103
spawn150 <spawn150@gmail.com>
2016-12-12 16:35:33 +0100

    login package created

---
+++ app/src/main/java/com/example/architecture/bad/myfigurecollection/login/LoginActivity.java
@@ -0,0 +1,10 @@
+package com.example.architecture.bad.myfigurecollection.login;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by spawn on 12/12/16.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+}
