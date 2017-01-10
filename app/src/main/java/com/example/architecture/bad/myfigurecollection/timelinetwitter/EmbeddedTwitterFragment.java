842de0a9150ccb43a9c44c526a332cc0b695790d
spawn150 <spawn150@gmail.com>
2017-01-10 18:58:34 +0100

    added swipe to refresh

--- app/src/main/java/com/example/architecture/bad/myfigurecollection/timelinetwitter/EmbeddedTwitterFragment.java
+++ app/src/main/java/com/example/architecture/bad/myfigurecollection/timelinetwitter/EmbeddedTwitterFragment.java
@@ -3,12 +3,19 @@ package com.example.architecture.bad.myfigurecollection.timelinetwitter;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.architecture.bad.myfigurecollection.R;
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.models.Tweet;
+import com.twitter.sdk.android.tweetui.TimelineResult;
 import com.twitter.sdk.android.tweetui.TweetTimelineListAdapter;
 import com.twitter.sdk.android.tweetui.UserTimeline;
 
@@ -42,14 +49,39 @@ public class EmbeddedTwitterFragment extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+
         final UserTimeline userTimeline = new UserTimeline.Builder()
                 .screenName(getContext().getString(R.string.mfc_twitter_screename))
+                .includeRetweets(true)
+                .includeReplies(true)
                 .build();
         final TweetTimelineListAdapter adapter = new TweetTimelineListAdapter.Builder(getContext())
                 .setTimeline(userTimeline)
                 .build();
         ListView embeddedTwitterTimelineListView = (ListView) view.findViewById(R.id.list_view_twitter_timeline);
         embeddedTwitterTimelineListView.setAdapter(adapter);
+
+        final SwipeRefreshLayout swipeRefreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.twitter_swipe_layout);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                swipeRefreshLayout.setRefreshing(true);
+                adapter.refresh(new Callback<TimelineResult<Tweet>>() {
+                    @Override
+                    public void success(Result<TimelineResult<Tweet>> result) {
+                        swipeRefreshLayout.setRefreshing(false);
+                    }
+
+                    @Override
+                    public void failure(TwitterException exception) {
+                        if (isAdded() && getActivity() != null) {
+                            swipeRefreshLayout.setRefreshing(false);
+                            Toast.makeText(getActivity(), "Error loading tweets...", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
 
     }
 }
