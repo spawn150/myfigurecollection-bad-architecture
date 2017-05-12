1656d1cb2c3b6ee01540efe4218a6a65d17abb06
spawn150 <spawn150@gmail.com>
2017-05-12 19:43:45 +0200

    added files for Search feature

---
+++ app/src/main/java/com/example/architecture/bad/myfigurecollection/search/SearchActivity.java
@@ -0,0 +1,16 @@
+package com.example.architecture.bad.myfigurecollection.search;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.architecture.bad.myfigurecollection.R;
+
+public class SearchActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
+
