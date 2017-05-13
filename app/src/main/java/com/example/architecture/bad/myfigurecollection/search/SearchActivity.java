968173141a084552bd4b6ff217781636b368296a
spawn150 <spawn150@gmail.com>
2017-05-13 15:53:07 +0200

    implemented navigation between calling activity and SearchActivity

--- app/src/main/java/com/example/architecture/bad/myfigurecollection/search/SearchActivity.java
+++ app/src/main/java/com/example/architecture/bad/myfigurecollection/search/SearchActivity.java
@@ -1,7 +1,9 @@
 package com.example.architecture.bad.myfigurecollection.search;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.example.architecture.bad.myfigurecollection.R;
 
@@ -12,6 +14,21 @@ public class SearchActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_search);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar ab = getSupportActionBar();
+        if (ab != null) {
+            ab.setDisplayHomeAsUpEnabled(true);
+        }
+
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return true;
     }
 
     @Override
